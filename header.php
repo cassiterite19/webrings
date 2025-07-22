f8a44b05052ead56ada99abf6f13250cdafa6fd4
cassiterite19 <alexandralilydavis@gmail.com>
2025-07-22 05:14:28 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<!HTML>
+<html>
+    <head>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="0">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>cassiterite's webrings</title>
+        <link href="/style.css" rel="stylesheet" type="text/css" media="all">
+    </head>
+    <body>
+        <header>
+            <h1>cassiterite's webrings</h1>
+            <nav>
+                <ul>
+                    <li><a href="/rings/">Webrings</a></li>
+                    <li><a href="/cats/">Categories</a></li>
+                    <li><a href="/contact/">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+
+
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/header.php';?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/sidebar.php';?>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/footer.php';?>
