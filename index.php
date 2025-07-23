6959a30af279ab43cc953986fb89e04e550ff85b
cassiterite19 <alexandralilydavis@gmail.com>
2025-07-22 21:21:54 -0400

    0722: added and modified onionring code

--- index.php
+++ index.php
@@ -7,5 +7,13 @@
         <p>i keep thinking up cool webring ideas and i am trying to figure out how to make them reality.</p>
         <p>if you want to join one or have an idea for a new ring, <a href="/contact/">contact me</a>.</p>
     </article>
+    <article>
+        <ul>
+            <li><a href="/rings/alt-h/">alterhuman webring</a></li>
+        </ul>
+    </article>
+    <footer>
+        <p>last updated 07/22/25</p>
+    </footer>
 </section>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/footer.php';?>
---
+++ rings/alt-h/index.php
@@ -0,0 +1,18 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/header.php';?>
+<link href="onionring/onionring.css" rel="stylesheet" type="text/css" media="all">
+<section>
+    <header>
+        <h2>alterhuman webring</h2>
+    </header>
+    <article>
+        <p>this webring is for alterhumans. (<a href="https://cass-iter-ite.com/about/alterhuman.php">what is alterhuman?</a>)</p>
+        <p>if you want to join, <a href="/contact/">contact me</a>.</p>
+    </article>
+    <article>
+        <script type="text/javascript" src="onionring/onionring-index.js"></script>
+    </article>
+    <footer>
+        <p>last updated 07/22/25</p>
+    </footer>
+</section>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/footer.php';?>
